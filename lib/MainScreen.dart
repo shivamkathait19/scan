4b1bd0b24b232ea05646b08ca7bb323aea2c265c
shivamkathait19 <kathaitshivam151@gmail.com>
2025-12-02 22:33:44 +0530

    Update MainScreen.dart

--- lib/MainScreen.dart
+++ lib/MainScreen.dart
@@ -10,7 +10,14 @@ class _MainscreenState extends State<Mainscreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+     appBar: AppBar(
+       title: Text("Scan itttt",style: TextStyle(
+           fontWeight: FontWeight.bold),),
+     backgroundColor: Colors.black,
+       foregroundColor: Colors.white,
+     )
 
+     ),
     );
   }
 }
