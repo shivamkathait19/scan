62a194f31017615f816a7d98e6e6f46d1683253f
shivamkathait19 <kathaitshivam151@gmail.com>
2025-12-11 21:44:55 +0530

    Update Scanproject.dart

--- lib/Scanproject.dart
+++ lib/Scanproject.dart
@@ -5,6 +5,10 @@ class projectX extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("HERE"),
+      ),
+    );
   }
 }
