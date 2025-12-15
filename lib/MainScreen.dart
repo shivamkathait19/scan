0a68db244933fa8f17b00eb2a3b9c7fbf62d33aa
shivamkathait19 <kathaitshivam151@gmail.com>
2025-12-15 21:14:38 +0530

    Update MainScreen.dart

--- lib/MainScreen.dart
+++ lib/MainScreen.dart
@@ -121,16 +121,4 @@ class Profilegpage extends StatelessWidget {
       ),
     );
   }
-} class page extends StatelessWidget {
-  const Settingpage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title:
-        Text("Setting",),
-      ),
-    );
-  }
 }
