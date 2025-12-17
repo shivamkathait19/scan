ad0a1d56dd417b7d91cf64dc61b418dc938bd725
shivamkathait19 <kathaitshivam151@gmail.com>
2025-12-17 21:45:31 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:scan_all/MainScreen.dart';
 import 'package:scan_all/SplashScreen.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         //colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: Splashscreen(),
+      home: Mainscreen(),
     );
   }
 }
