d141d5c5aceb9236026e47580e804ebd8601ae2f
shivamkathait19 <kathaitshivam151@gmail.com>
2025-12-25 21:50:24 +0530

    Update MakeScreen.dart

--- lib/MakeScreen.dart
+++ lib/MakeScreen.dart
@@ -8,14 +8,33 @@ class projectX extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
-        title: Text("Scan project ",style: TextStyle(backgroundColor: Colors.black,fontSize: 20),),
+        title: Text("Try  project ",style: TextStyle(backgroundColor: Colors.black,fontSize: 20),),
       ),
-      body: Column(
-        children: [
-  Text (" scaner ")
-
-        ],
-      ),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        child: Column(
+          children: [
+            SizedBox(
+              height: 60,
+              width: double.infinity,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (ctx,index){
+                  return Container(
+                    margin: EdgeInsets.all(5),
+                    width: 80,
+                    height: 45,
+                    decoration: BoxDecoration(
+                      color: Colors.white
+                    ),
+                  );
+                }
+              )
+            )
+          ],
+        ),
+      )
     );
   }
 }
